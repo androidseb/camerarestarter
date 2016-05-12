a33174eeaa8cc42a31b3a4a0c6cd2cda8511a794
androidseb <theandroidseb@gmail.com>
2016-05-12 08:28:30 -0400

    added the "auto launch camera" feature

--- app/src/main/java/com/exlyo/camerarestarter/MainActivity.java
+++ app/src/main/java/com/exlyo/camerarestarter/MainActivity.java
@@ -1,12 +1,18 @@
 package com.exlyo.camerarestarter;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.Toast;
 
 import java.io.BufferedReader;
@@ -14,6 +20,9 @@ import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
+	private static final String PREFS_FILE_NAME = "camera_restarter_prefs";
+	private static final String PREF_KEY_CAMERA_AUTO_LAUNCH = "camera_auto_launch";
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -22,6 +31,14 @@ public class MainActivity extends AppCompatActivity {
 			@Override
 			public void onClick(final View v) {
 				restartButtonAction(MainActivity.this);
+			}
+		});
+		final CheckBox autoLaunchCheckBox = (CheckBox) findViewById(R.id.auto_launch_camera_checkbox);
+		autoLaunchCheckBox.setChecked(MainActivity.isAutoCameraLaunchEnabled(this));
+		autoLaunchCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+			@Override
+			public void onCheckedChanged(final CompoundButton buttonView, final boolean isChecked) {
+				MainActivity.setAutoCameraLaunchEnabled(MainActivity.this, autoLaunchCheckBox.isChecked());
 			}
 		});
 		findViewById(R.id.help_button).setOnClickListener(new View.OnClickListener() {
@@ -35,11 +52,29 @@ public class MainActivity extends AppCompatActivity {
 	public static void restartButtonAction(final Context _context) {
 		try {
 			runRestartCameraShellCommand();
+			if (MainActivity.isAutoCameraLaunchEnabled(_context)) {
+				final Intent cameraStartIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+				cameraStartIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+				_context.startActivity(cameraStartIntent);
+			}
 			showToastMessage(_context, _context.getString(R.string.camera_restared_successfully));
 		} catch (Throwable t) {
 			t.printStackTrace();
 			showToastMessage(_context, _context.getString(R.string.camera_restart_failed, t.getMessage()));
 		}
+	}
+
+	private synchronized static boolean isAutoCameraLaunchEnabled(final Context _context) {
+		final SharedPreferences sharedPreferences = _context.getSharedPreferences(PREFS_FILE_NAME, MODE_PRIVATE);
+		return sharedPreferences.getBoolean(PREF_KEY_CAMERA_AUTO_LAUNCH, true);
+	}
+
+	@SuppressLint("CommitPrefEdits")
+	private synchronized static void setAutoCameraLaunchEnabled(final Context _context, final boolean _enabled) {
+		final SharedPreferences sharedPreferences = _context.getSharedPreferences(PREFS_FILE_NAME, MODE_PRIVATE);
+		final SharedPreferences.Editor edit = sharedPreferences.edit();
+		edit.putBoolean(PREF_KEY_CAMERA_AUTO_LAUNCH, _enabled);
+		edit.commit();
 	}
 
 	private static void runRestartCameraShellCommand() throws Throwable {
